5d1b267aff10b907323bb5fd83984413737fc22b
Shivam Patel <patelsd96@gmail.com>
2021-03-09 22:43:41 -0500

    trying to fix the search useEffect

--- react-albums/src/components/LiveSearch.js
+++ react-albums/src/components/LiveSearch.js
@@ -1,11 +1,25 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 import SearchBar from "components/SearchBar";
 import Results from "components/Results";
+const axios = require('axios');
 
 export default function LiveSearch(props) {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+  useEffect(() => {
+    const testURL = `https://itunes.apple.com/search?term=${term}&country=CA&media=music&entity=album&attribute=artistTerm`
+    axios.get(testURL)
+      .then(function(response){
+        // handle success
+        console.log(response.data.results)
+        setResults([...response.data.results])
+      })
+      .catch(function(error){
+        // handle error
+        console.log(error)
+      });
+  }, [term])
 
   return (
     <Fragment>
